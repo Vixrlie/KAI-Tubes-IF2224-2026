8a20c7f6c032d91e4ddb62d9f964f8c7fc25fcae
BryannPPH <bryanpratama0111@gmail.com>
2026-05-02 16:57:40 +0700

    feat: add declarations to parser

--- src/parser/parser.h
+++ src/parser/parser.h
@@ -36,13 +36,28 @@ private:
 
     bool isAtEnd() const;
     bool check(TokenType type) const;
+    bool check(TokenType type, std::size_t offset) const;
     const Token &peek() const;
+    const Token *peek(std::size_t offset) const;
     const Token &advance();
     Token consume(TokenType type, const std::string &expectedName);
     std::unique_ptr<ParseTreeTerminalNode> makeTerminalNode(const Token &token) const;
     [[noreturn]] void throwSyntaxError(const std::string &expectedName) const;
+    bool isRangeStart() const;
 
     std::unique_ptr<DeclarationPartNode> parseDeclarationPart();
+    std::unique_ptr<ConstDeclarationNode> parseConstDeclaration();
+    std::unique_ptr<ConstantNode> parseConstant();
+    std::unique_ptr<TypeDeclarationNode> parseTypeDeclaration();
+    std::unique_ptr<VarDeclarationNode> parseVarDeclaration();
+    std::unique_ptr<IdentifierListNode> parseIdentifierList();
+    std::unique_ptr<TypeNode> parseType();
+    std::unique_ptr<ArrayTypeNode> parseArrayType();
+    std::unique_ptr<RangeNode> parseRange();
+    std::unique_ptr<EnumeratedNode> parseEnumerated();
+    std::unique_ptr<RecordTypeNode> parseRecordType();
+    std::unique_ptr<FieldListNode> parseFieldList();
+    std::unique_ptr<FieldPartNode> parseFieldPart();
     std::unique_ptr<CompoundStatementNode> parseCompoundStatement();
     std::unique_ptr<StatementListNode> parseStatementList();
 };
