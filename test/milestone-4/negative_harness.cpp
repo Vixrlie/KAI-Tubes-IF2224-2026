0b4dddd58500998bab60f077f5cbb3ece8bb6f51
parkuskus <ksmaachmad@gmail.com>
2026-05-28 16:50:24 +0700

    feat: add negative test cases for out-of-bounds memory access and division/modulo by zero

--- test/milestone-4/negative_harness.cpp
+++ test/milestone-4/negative_harness.cpp
@@ -76,5 +76,74 @@ int main()
         }
     }
 
+    // Invalid LOD address (out of bounds)
+    {
+        std::vector<CodeGen::Instruction> prog;
+        CodeGen::Instruction i0;
+        i0.op = CodeGen::OpCode::INT;
+        i0.level = 0;
+        i0.operand = 3;
+        CodeGen::Instruction i1;
+        i1.op = CodeGen::OpCode::LOD;
+        i1.level = 0;
+        i1.operand = 999;
+        CodeGen::Instruction i2;
+        i2.op = CodeGen::OpCode::RET;
+        i2.level = 0;
+        i2.operand = 0;
+        prog.push_back(i0);
+        prog.push_back(i1);
+        prog.push_back(i2);
+
+        Interpreter::Interpreter interp;
+        auto res = interp.run(prog);
+
+        std::ofstream out("test/milestone-4/invalid_lod_output.txt");
+        out << "=== Interpreter Output ===\n"
+            << res.output << "\n";
+        out << "=== Interpreter Errors ===\n";
+        for (const auto &e : res.errors)
+        {
+            out << "- " << e << "\n";
+        }
+    }
+
+    // Invalid STO address (out of bounds write)
+    {
+        std::vector<CodeGen::Instruction> prog;
+        CodeGen::Instruction i0;
+        i0.op = CodeGen::OpCode::INT;
+        i0.level = 0;
+        i0.operand = 3;
+        CodeGen::Instruction i1;
+        i1.op = CodeGen::OpCode::LIT;
+        i1.level = 0;
+        i1.operand = 5;
+        CodeGen::Instruction i2;
+        i2.op = CodeGen::OpCode::STO;
+        i2.level = 0;
+        i2.operand = 9999;
+        CodeGen::Instruction i3;
+        i3.op = CodeGen::OpCode::RET;
+        i3.level = 0;
+        i3.operand = 0;
+        prog.push_back(i0);
+        prog.push_back(i1);
+        prog.push_back(i2);
+        prog.push_back(i3);
+
+        Interpreter::Interpreter interp;
+        auto res = interp.run(prog);
+
+        std::ofstream out("test/milestone-4/invalid_sto_output.txt");
+        out << "=== Interpreter Output ===\n"
+            << res.output << "\n";
+        out << "=== Interpreter Errors ===\n";
+        for (const auto &e : res.errors)
+        {
+            out << "- " << e << "\n";
+        }
+    }
+
     return 0;
 }
